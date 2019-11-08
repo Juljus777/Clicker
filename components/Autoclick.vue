ee05f365ce908ac51661b9c1a2b3307ef9ea7489
Anna Andreas <anna.andreas@tptlive.ee>
2019-11-08 11:15:14 +0200

    first commit

---
+++ components/Autoclick.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="buyAutoClick" class="button is-primary" :disabled="$store.state.counter<this.cost" >{{this.name}}</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "autoclick",
+        props: ['name', 'cost', 'cps'],
+        methods: {
+            buyAutoClick(){
+                this.$store.dispatch('buyAutoClick', cps);
+                this.$store.dispatch('buyItem', cost);
+          },
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
