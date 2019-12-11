b180dbe14d4429057da0b2f03556d64bd9204734
Juljus777 <42836212+Juljus777@users.noreply.github.com>
2019-12-11 14:58:25 +0200

    Add files via upload

--- store/index.js
+++ store/index.js
@@ -1,30 +1,34 @@
 export const state = () => ({
   counter: 0,
   perSecond: 0,
+  perClick: 1,
 });
 
 export const mutations = {
   INCREMENT_COUNT(state, value){
     state.counter += value;
   },
-  INCREMENT_PER_SECOND(state,value){
+  INCREMENT_PER_SECOND(state, value){
     state.perSecond += value;
   },
-  REMOVE_COOKIES(state,cost){
+  REMOVE_COOKIES(state, cost){
     state.counter -= cost;
   },
-  SET_ITEM_VALUE(state, cost){
-    state.c
+  ADD_PER_CLICK(state, value){
+    state.perClick += value;
   }
 };
 export const actions = {
   increment({commit}, value){
     commit('INCREMENT_COUNT', value);
   },
-  buyAutoClick({commit}, cps){
+  addToCPS({commit}, cps){
     commit('INCREMENT_PER_SECOND', cps);
   },
   buyItem({commit}, cost){
     commit('REMOVE_COOKIES', cost);
+  },
+  addPerClick({commit}, value){
+    commit('ADD_PER_CLICK', value);
   }
 }
