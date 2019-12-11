a16748e38d067c46c54591484d27cf918aed06ad
Juljus777 <42836212+Juljus777@users.noreply.github.com>
2019-12-11 14:57:16 +0200

    Add files via upload

--- components/Autoclick.vue
+++ components/Autoclick.vue
@@ -1,22 +1,37 @@
 <template>
   <div>
-    <button @click="buyAutoClick" class="button is-primary" :disabled="$store.state.counter<this.cost" >{{this.name}} This costs: {{this.cost}}</button>
+    <button @click=" buyFromShop" class="button is-primary" :disabled="$store.state.counter<this.cost">{{this.name}} costs: {{this.cost}}</button>
   </div>
 </template>
 
 <script>
     export default {
-        name: "autoclick",
-        props: ['name', 'cost', 'cps'],
+        name: "Autoclick",
+        props: ['name', 'cost', 'cps', 'type', 'buyCount', 'butLimit'],
         methods: {
-            buyAutoClick(){
-                this.$store.dispatch('buyAutoClick', this.cps);
-                this.$store.dispatch('buyItem', this.cost);
+          buyFromShop(){
+            this.buyCount++;
+            if(this.type==='automatic')
+            {
+              this.$store.dispatch('addToCPS', this.cps);
+              this.$store.dispatch('buyItem', this.cost);
+              this.cost = Math.round(this.cost*1.5);
+            }
+            else if(this.type==='manual'){
+              this.$store.dispatch('buyItem', this.cost);
+              this.$store.dispatch('addPerClick', this.cps);
+              this.cost = Math.round(this.cost*5);
+            }
+            else if(this.type==='special'){
+              this.cost = Math.round(this.cost*10);
+            }
           },
       }
     }
 </script>
 
 <style scoped>
-
+  button{
+    margin:10px;
+  }
 </style>
--- components/shop.vue
+++ components/shop.vue
@@ -1,10 +1,23 @@
 <template>
   <aside class="menu has-background-grey-light">
     <p class="menu-label">
-      General
+      General items:
     </p>
     <ul class="menu-list">
-      <upgrades name="Autoclick" :cost="10" :cps="1"></upgrades>
+      <upgrades name="Auto click" :type="'automatic'" :cost="10" :cps="1"></upgrades>
+      <upgrades name="Grandma" :type="'automatic'" :cost="100" :cps="5"></upgrades>
+    </ul>
+    <p class="menu-label">
+      Upgrade items:
+    </p>
+    <ul class="menu-list">
+      <upgrades name="Double click" :type="'manual'" :cost="10" :cps="1"></upgrades>
+    </ul>
+    <p class="menu-label">
+      Special items:
+    </p>
+    <ul class="menu-list">
+      <upgrades name="Extra spicy yield" :type="'special'" :cost="500" :cps="50"></upgrades>
     </ul>
   </aside>
 </template>
