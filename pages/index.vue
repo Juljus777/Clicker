c6909895d5c32c2578a29fce365a6ff1d32e6f0c
Juljus777 <42836212+Juljus777@users.noreply.github.com>
2019-12-11 14:58:02 +0200

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,12 @@
     <div class="container">
       Cookies owned: {{$store.state.counter}}
       <img @click="addCookie" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/2ChocolateChipCookies.jpg/1280px-2ChocolateChipCookies.jpg">
-      <button :disabled="$store.state.counter<10" @click="addPerSecond" class="button is-primary">Auto click</button>
       <p>Auto clicks per second: {{$store.state.perSecond}} </p>
     </div>
   </div>
 </template>
 
 <script>
-
 import Upgrades from "../components/Autoclick";
 export default {
     components: {Upgrades},
@@ -22,7 +20,7 @@ export default {
     },
   methods: {
       addCookie(){
-          this.$store.dispatch('increment', 1);
+          this.$store.dispatch('increment', this.$store.state.perClick);
       },
       addPerSecond(){
           this.$store.dispatch('incrementPerSecond', 1);
